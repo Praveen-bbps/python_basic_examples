b3506e2ad642a739637d2588ad81376104e82460
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-11-09 17:02:49 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,10 +1,20 @@
+#varibale declaration
 a=3
 b=5
 #arithmetic operators
+#Addition
+
+
 print(a+b)
+#Subtraction
 print(b-a)
+#Multiplication
 print(a*b)
+#Division
 print(b/a)
+#Exponentiation
 print(b**a)
+#Intiger Division
 print(b//a)
+#modulus
 print(b%a)
