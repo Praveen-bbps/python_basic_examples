589347c9576da6e7d7b93d881d4d255de302d348
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-11-09 18:18:04 +0530

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,13 @@
+#conversion of data types
+
+age=25.5  # its a float
+sal=10000 # its a integer  
+actual_age=int(age)  #  conversion of float to int
+print(actual_age)
+print(type(actual_age))
+print(type(sal))
+actual_sal=float(sal)  #  conversion of int to float
+print(type(actual_sal))
+print(actual_sal)
+
+
