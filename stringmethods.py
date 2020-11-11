17369e36dc6f09ec834e532c7ec7bd9539e1605b
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-11-11 18:19:10 +0530

    Add files via upload

---
+++ stringmethods.py
@@ -0,0 +1,13 @@
+#String Methods
+first_name="Praveen"
+last_name='kumar'
+print(first_name[6])
+#upper() method converts a string into upper case
+print(first_name.upper())
+print(last_name.capitalize())
+c = first_name.upper()
+print(c)
+para="I love India and I love Apple"
+print(para.count("love"))
+
+
