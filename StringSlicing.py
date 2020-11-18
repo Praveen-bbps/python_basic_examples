8cd307ae9d24a2b8937ddde6f6ec95397d084399
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-11-18 18:22:46 +0530

    Add files via upload

---
+++ StringSlicing.py
@@ -0,0 +1,16 @@
+a = "apple"
+b = "fruit"
+c = a+b
+print(c)
+print(3*a)
+# to print Fruit Apple
+print(b.capitalize() + " " + a.capitalize())
+# slicing
+print(a[:2])
+print(a[1:3])
+print(a[0:])
+print(a[:])
+
+
+
+
