5b6e850c513f22b8c4b409f80319d0817d6db8b5
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-11-20 18:16:01 +0530

    Add files via upload

---
+++ ListDemo.py
@@ -0,0 +1,13 @@
+# List demo
+month_names = ["jan", "feb", "mar", 2021,5]
+# finding the type of variable
+print(type(month_names))
+# finding the length of list
+print(len(month_names))
+# printing the last value in list
+print(month_names[len(month_names)-1])
+print(month_names[-1])
+print(month_names[4])
+# printing center position value from the list
+print(month_names[len(month_names)//2])
+
