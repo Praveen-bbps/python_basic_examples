aca41b7e63a61ad31a677c25048a14c659e74b32
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-11-24 20:36:02 +0530

    Add files via upload

---
+++ mutable_demo.py
@@ -0,0 +1,25 @@
+# Strings are immutable
+a="bbps"
+print(a[2])
+# make the below line comment to remove the error
+a[2]="q"
+# lists are mutable
+months=["jan","feb","mar"]
+print(months[2])
+months[2]="apr"
+print(months[2])
+
+myscore=[20,15,80,25,90]
+myname=["bbps", "school",25]
+# ascending order
+print(sorted(myscore))
+# decending order
+print(sorted(myscore,reverse=True))
+# make the below line comment to remove the error
+print(min(myname))
+print(min(myscore))
+
+
+
+
+
