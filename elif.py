7abc7428e3530c90dd6a0cee42a069be3a88a141
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-12-02 18:27:48 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,11 @@
+# Multiple conditions
+
+gender = 'g'
+if gender == 'b':
+    print("you are a boy")
+elif gender == 'g':
+    print("you are a girl")
+else:
+    print("you are a transgender")
+    
+   
