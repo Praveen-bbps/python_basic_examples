1757a74f87c5cf41f20f59e0f1738bc799abc89c
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-12-07 17:39:39 +0530

    Add files via upload

---
+++ squareTurtleDemo.py
@@ -0,0 +1,16 @@
+#Drawing Square using turtle library
+import turtle
+turtle.color("red","blue")
+
+turtle.begin_fill()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+turtle.end_fill()
+turtle.done()
