9374578af7e44df7308f5b165f85ba55c6de587a
Praveen-bbps <74067365+Praveen-bbps@users.noreply.github.com>
2020-12-18 21:15:45 +0530

    Add files via upload

---
+++ function_return.py
@@ -0,0 +1,10 @@
+# Area of a rectangle using functions
+def area(l,b):
+    return l*b
+
+# enter length and breadth of the rectangle
+length = int(input("Enter the length"))
+breadth = int(input("Enter the breadth"))
+# call the function
+c = area(length,breadth)
+print(c)
